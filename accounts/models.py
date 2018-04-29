cc2cc873ca3a7a4bd2d9188ac359e3562ad4a097
adetbekov <dosya@inbox.ru>
2018-04-29 13:19:10 +0600

    Add accounts

---
+++ accounts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+import os
+
+
+def upload_full_image(instance, filename):
+    return "avatars/%s%s%s" % (instance.id, hashing(), os.path.splitext(filename)[1])
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='profile')
+    birth = models.DateField(null=True, blank=True)
+    phone = models.CharField(max_length=12, null=True, blank=True)
+    
+
+User.profile = property(lambda u: Profile.objects.get_or_create(user=u)[0])
