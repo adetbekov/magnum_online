4a4cc96ff2436b8f093510e5c526f574d0875954
adetbekov <dosya@inbox.ru>
2018-04-29 15:58:01 +0600

    Edit Products and Transactions admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Product, Transaction
 
-admin.site.register(Product)
-admin.site.register(Transaction)
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ('name','created','price','available')
+    list_filter=['available']
+    search_fields=['name','price']
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    model = Transaction
+    list_display = ('user','created','status','qr_hash')
+    list_filter=['status']
+    search_fields=['user','qr_hash']
+    filter_horizontal = ('cart',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Transaction, TransactionAdmin)
--- products/models.py
+++ products/models.py
@@ -4,14 +4,38 @@ from django.contrib.auth.models import User
 from magnum_online.functions import randhash6
 
 
+STATUS_CHOICE = {
+	('I', 'Incomplete'),
+	('S', 'Completed'),
+	('R', 'Rejected'),
+	('C', 'Canceled'),
+}
+
+
 class Product(models.Model):
     name = models.CharField(max_length=30)
     price = models.IntegerField()
+    created = models.DateTimeField(default=datetime.now)
     available = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return "<product-{}>".format(self.id)
 
 
 class Transaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     cart = models.ManyToManyField(Product)
+    status = models.CharField(max_length=3, choices=STATUS_CHOICE, default="I")
     created = models.DateTimeField(default=datetime.now)
     qr_hash = models.CharField(max_length=6, default=randhash6, unique=True)
+
+    class Meta:
+        verbose_name = "Транзакция"
+        verbose_name_plural = "Транзакции"
+
+    def __str__(self):
+        return "<transaction-{}>".format(self.qr_hash)
