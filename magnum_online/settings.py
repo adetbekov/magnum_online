61980bdf895292889c6885e9b9150aad8cebe2ab
adetbekov <dosya@inbox.ru>
2018-04-29 13:41:55 +0600

    Update settings

--- magnum_online/settings.py
+++ magnum_online/settings.py
@@ -127,4 +127,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+   os.path.join(BASE_DIR,"static"),
+)
+
+MEDIA_ROOT = 'media'
+MEDIA_URL = '/media/'
+
 AUTH_PROFILE_MODULE = 'accounts.Profile'
